726b86fa55e57cfbb00bce4aad87529d66524c99
Khalid Mohamed Elborai <accnew820@gmail.com>
2021-11-30 00:34:46 +0200

    React Blog

---
+++ src/config.js
@@ -0,0 +1,32 @@
+export const config = {
+  // Your Github Converted Token To Know How To Get Your Token Look at Readme.md
+  githubConvertedToken: "Z2hwX3NzQVhTeE9vVnJlVUZQanlSM0VnSjA1UFhyRzM2QzIzRTZWMQ==",
+
+  // Your Github UserName
+  githubUserName: "khalidelborai",
+
+  // Your Github Repo Name Where You Have your issues as Blog
+  githubRepo: "khalidelboray.github.io",
+
+  // Set it to true if you have a Github app to add to this project
+  // and fill the client ID & secret
+  enableOAuth: true,
+  OAuthClientID: 'Iv1.aec735d4e1cc4e9e',
+  OAuthSecret: '81feb450888ebfee9bce6fdbc621d4a3188a57ad',
+
+  // Your Personal Blog Title
+  title : "Khalid Elborai" ,
+
+  // Your Personal Blog Subtitle
+  subtitle : "Software Developer",
+
+  // Header customization
+  header: {
+    backgroundColor: '#f1f6f8', // can be a CSS gradient
+    backgroundColorDark: '#4C566A',
+    titleColor: '#ff5252',
+    titleColorDark: '#16a085',
+    subtitleColor: '#37474f',
+    subtitleColorDark: '#D8DEE9',
+  },
+};
